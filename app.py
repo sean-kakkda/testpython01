e63200fb7e24306c21f44893e4684d7c5c868a16
sean-kakkda <seankakkda2@gmail.com>
2024-07-19 20:14:58 -0700

    first

---
+++ app.py
@@ -0,0 +1,37 @@
+#print("Hello Student")
+
+#print("Single 'quote' inside double quote")
+#print('Double "quote" inside single quote')
+#print("single \'quote\' and \"double\" quote \ninside quote")
+
+#name=input("Whate is your name?")
+#print("welcome" +name)
+
+#old=input("how are you?")
+#print("i'm" +old)
+#
+#Test variable
+# print("======Welcome Amount======")
+#
+# qty1=int(input("Input Qty1 : "))
+# qty2=int(input("Input Qty2 : "))
+# price=float(input("Input Price : "))
+# total_qty=qty1+qty2
+# print("TotalQty" +str(total_qty))
+# amount=total_qty*price
+# print("Total Amount+" +str(amount))
+
+# There are two variable ,a and b from input
+# a = input("a : ")
+# b = input("b : ")
+# c=a
+# a=b
+# b=c
+# print ("a:"+a)
+# print("b:" +b)
+
+print("===Welcome to the band name generator===")
+city = input("Which city did you grow up in ?\n")
+name_pat = input("What is the name of a pet?\n")
+print("Your band name could be "+city+" "+name_pat)
+
