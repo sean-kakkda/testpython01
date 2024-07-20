cd81221d364db247552a48702cebd236739b3655
sean-kakkda <seankakkda2@gmail.com>
2024-07-19 21:29:58 -0700

    frist

--- testdata.py
+++ testdata.py
@@ -1,10 +1,25 @@
-str="Hello World"
-print(str)
-print(str[0])
-print(str[2:5])
-print(str[2:])
-print(str[2:1])
-print(str*10)
-print(str+"From BBU")
+str_text="Hello World"
+print(str_text)
+print(str_text[0])
+print(str_text[2:5])
+print(str_text[2:])
+print(str_text[2:1])
+print(str_text*10)
+print(str_text+"From BBU")
+print(type(str_text))
 
 
+#Number
+a=1_000_000
+print(a)
+print(type(a))
+b=13.04
+print(type(b))
+print(b)
+
+val1=int("10")
+val2=int("20")
+total=val1+val2
+print(type(total))
+print("Total=" +str(total))
+print(len(str(total)))
